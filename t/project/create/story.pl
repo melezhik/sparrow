afc9b5612f5be669bd756ba93767cc6a842df615
Alexey Melezhik <melezhik@gmail.com>
2015-11-30 17:55:13 +0300

    remove project action

--- t/project/create/story.pl
+++ t/project/create/story.pl
@@ -1,3 +1,8 @@
 print `sparrow project foo100 remove`;
 print `sparrow project foo100 create`;
 
+use Sparrow::Constants;
+
+-d sparrow_root."/projects/foo100" and print "directory projects/foo100 exists\n";
+
+
--- t/project/remove/story.pl
+++ t/project/remove/story.pl
@@ -2,3 +2,4 @@ print `sparrow project foo100 remove`;
 print `sparrow project foo100 create`;
 print `sparrow project foo100 remove`;
 
+-d sparrow_root."/projects/foo100" or print "directory projects/foo100 removed\n";

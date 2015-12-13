cc228eb3b3b6e8d5d4088e1ac46031bac06d0237
Alexey Melezhik <melezhik@gmail.com>
2015-12-13 22:04:41 +0300

    checkset

--- t/checkpoint-set/story.pl
+++ t/checkpoint-set/story.pl
@@ -2,5 +2,9 @@ use lib 't/lib/';
 
 use test;
 
-print Dumper(cp_get('foo100','bar'));
+my $d  = cp_get('foo100','bar');
+
+print "base_url: $d->{base_url}\n";
+print "plugin: $d->{plugin}\n";
+
 print "OK\n";

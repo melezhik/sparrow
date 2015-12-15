28dae276a3bfa7528c669d41788bf308625131b1
Alexey Melezhik <melezhik@gmail.com>
2015-12-15 16:42:12 +0300

    fix-tests

--- t/checkpoint-set/story.pm
+++ t/checkpoint-set/story.pm
@@ -1,5 +1,7 @@
 run_story('project/create');
 run_story('project/checkpoint/create');
+run_story('plg/install');
 run_story('project/checkpoint/set', { url => '127.0.0.1'});
-run_story('project/checkpoint/set', { url => '127.0.0.1', plugin => 'test777' });
+
+run_story('project/checkpoint/set', { url => '127.0.0.1', plugin => 'foo-test' });
 

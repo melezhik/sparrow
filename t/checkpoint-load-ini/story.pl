94f33d493a0f1518506c0b2b5c6a4e7b792a1a0d
Alexey Melezhik <melezhik@gmail.com>
2016-01-18 21:55:55 +0300

    ini-files

--- t/checkpoint-load-ini/story.pl
+++
@@ -1,7 +0,0 @@
-use lib 't/lib/';
-
-use test;
-
-my $cmd = "sparrow check load_ini foo100 bar ".project_root_dir()."/suite.ini"; 
-print `$cmd`;
-print "OK\n";
--- t/checkpoint-load-ini/story.pm
+++ t/checkpoint-load-ini/story.pm
@@ -1,3 +1,17 @@
 run_story('project/create');
 run_story('project/checkpoint/create');
 
+use lib 't/lib/';
+
+use test;
+
+my $cmd = "sparrow check load_ini foo100 bar ".project_root_dir()."/suite.ini";
+set_stdout(`$cmd`);
+set_stdout('OK');
+my $f = sparrow_root().'/projects/foo100/checkpoints/bar/suite.ini';
+open F, $f or die $!;
+while (my $s = <F>){
+    chomp $s;
+    set_stdout($s);
+}
+close F;

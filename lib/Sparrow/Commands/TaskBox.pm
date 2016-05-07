0a5518e78f2a9e8a378b221db69aed9fb72df0ad
Alexey Melezhik <melezhik@gmail.com>
2016-05-07 16:33:52 +0300

    add task box module

---
+++ lib/Sparrow/Commands/TaskBox.pm
@@ -0,0 +1,24 @@
+package Sparrow::Commands::TaskBox;
+
+use strict;
+
+use base 'Exporter';
+
+use Sparrow::Constants;
+use Sparrow::Misc;
+use Carp;
+use File::Path;
+use YAML;
+use File::Copy;
+
+our @EXPORT = qw{
+
+    box_run
+
+};
+
+sub box_run {
+}
+
+1;
+

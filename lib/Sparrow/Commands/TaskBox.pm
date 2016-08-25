1c7f3dead208c9f96ab4579bdd96b05bd01539bd
Alexey Melezhik <melezhik@gmail.com>
2016-08-25 11:50:51 +0300

    taskbox - improve console output

--- lib/Sparrow/Commands/TaskBox.pm
+++ lib/Sparrow/Commands/TaskBox.pm
@@ -74,7 +74,7 @@ sub box_run {
 
       $i++;
 
-      print "\n[running task <$task->{task}>] ... \n";
+      print "\nrunning task <$task->{task}> ... \n";
 
       my $path = sparrow_root()."/cache/task_$i.json";
 

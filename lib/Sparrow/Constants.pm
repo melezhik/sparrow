cbdfe3a6d93154e61f56f55dbc330f5392ddb22e
Alexey Melezhik <melezhik@gmail.com>
2018-09-21 14:40:07 +0000

    portable homedir

--- lib/Sparrow/Constants.pm
+++ lib/Sparrow/Constants.pm
@@ -17,6 +17,11 @@ our @EXPORT = qw {
 
 # sparrow configuration file
 
+sub home_dir {
+
+  $^O  =~ 'MSWin' ? "C:/Users/".getlogin() : $ENV{HOME}
+}
+
 sub safe_env {
 	my $v = shift;
 	$v=~s/\s//g if $^O  =~ 'MSWin'; 
@@ -24,19 +29,19 @@ sub safe_env {
 }
 
 sub sparrow_conf_file () {  
-  $ENV{SPARROW_CONF_PATH} || do { $ENV{SPARROW_ROOT} ? safe_env("$ENV{SPARROW_ROOT}/sparrow.yaml") : safe_env("$ENV{HOME}/sparrow.yaml") }
+  $ENV{SPARROW_CONF_PATH} || do { $ENV{SPARROW_ROOT} ? safe_env("$ENV{SPARROW_ROOT}/sparrow.yaml") : safe_env(home_dir()."/sparrow.yaml") }
 };
 
 # sparrow index - for public plugins comes from SparrowHub
-sub spi_file () {  safe_env($ENV{SPARROW_ROOT}  ? "$ENV{SPARROW_ROOT}/sparrow.index" : "$ENV{HOME}/sparrow.index") };
+sub spi_file () {  safe_env($ENV{SPARROW_ROOT}  ? "$ENV{SPARROW_ROOT}/sparrow.index" : home_dir()."/sparrow.index") };
 
 # sparrow custom index - for custom plugins comes from custom repository (see "repo" in sparrow ini file )
-sub spci_file () {  safe_env($ENV{SPARROW_ROOT}  ? "$ENV{SPARROW_ROOT}/sparrow.custom.index" : "$ENV{HOME}/sparrow.custom.index") };
+sub spci_file () {  safe_env($ENV{SPARROW_ROOT}  ? "$ENV{SPARROW_ROOT}/sparrow.custom.index" : home_dir()."/sparrow.custom.index") };
 
 # sparrow list index - for private plugins comes from github repositories
-sub spl_file () {  safe_env($ENV{SPARROW_ROOT}  ? "$ENV{SPARROW_ROOT}/sparrow.list" : "$ENV{HOME}/sparrow.list") };
+sub spl_file () {  safe_env($ENV{SPARROW_ROOT}  ? "$ENV{SPARROW_ROOT}/sparrow.list" : home_dir()."/sparrow.list") };
 
-sub sparrow_root () { safe_env($ENV{SPARROW_ROOT} || "$ENV{HOME}/sparrow") };
+sub sparrow_root () { safe_env($ENV{SPARROW_ROOT} || home_dir()."/sparrow") };
 
 # SparrowHub API 
 sub sparrow_hub_api_url () { $ENV{sparrow_hub_api_url} || 'https://sparrowhub.org' };
